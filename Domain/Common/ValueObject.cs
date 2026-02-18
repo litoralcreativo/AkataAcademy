27f9604c94563c11bbcbecc6e58a905059fbb4a0
Litoral Creatives <litoralcreatives@gmail.com>
2026-02-18 18:36:36 -0300

    Add project files.

---
+++ Domain/Common/ValueObject.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public abstract class ValueObject
+    {
+        protected abstract IEnumerable<object> GetEqualityComponents();
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not ValueObject other)
+                return false;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            return GetEqualityComponents()
+                .SequenceEqual(other.GetEqualityComponents());
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+
+                foreach (var obj in GetEqualityComponents())
+                {
+                    hash = hash * 23 + (obj != null ? obj.GetHashCode() : 0);
+                }
+
+                return hash;
+            }
+        }
+
+        public static bool operator ==(ValueObject? a, ValueObject? b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(ValueObject? a, ValueObject? b)
+        {
+            return !(a == b);
+        }
+    }
+}
