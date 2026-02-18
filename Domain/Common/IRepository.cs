5a4dd574a59ad07cf72df8b906671da0005ee830
Litoral Creatives <litoralcreatives@gmail.com>
2026-02-18 18:57:31 -0300

    Add domain model base classes and interfaces
    Introduced foundational classes and interfaces in the `Domain.Common` namespace to support the domain model:
    
    - Added `AggregateRoot` class as a marker for aggregate roots.
    - Added `DomainException` for custom domain-related errors.
    - Added `Entity` abstract class with unique ID, domain event handling, and equality logic.
    - Added `IDomainEvent` interface for domain event metadata.
    - Added `IRepository<T>` interface defining repository operations.

---
+++ Domain/Common/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public interface IRepository<T>
+    where T : AggregateRoot
+    {
+        T GetById(Guid id);
+        bool Exists(Guid id);
+        void Add(T course);
+        void Remove(T course);
+    }
+}
